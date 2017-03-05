83c2993c0573f18eb13d7b5c92dcf356d910430d
ponydelrey <ann.cz3c@gmail.com>
2017-03-05 23:47:40 +0100

    Adding routing and login screen

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
-var BBApp = angular.module('BBApp', [])
+var BBApp = angular.module('BBApp', ['ngRoute']);
+
+BBApp.config(function($routeProvider){
+    $routeProvider
+        .when('/', {
+            templateUrl: 'home.html'
+        })
+        .when('/login', {
+            templateUrl: 'cart.html'
+        })
+
+});
+
 
 BBApp.controller('MainController', ['$scope', function ($scope) {
 
 $scope.my = 'Galeria';
+
+
 }])
