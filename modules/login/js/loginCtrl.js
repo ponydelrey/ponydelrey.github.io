44dc1dff8445785599b022c2138794aaafb58731
ponydelrey <ann.cz3c@gmail.com>
2017-03-14 22:15:01 +0100

    Updated code

--- modules/login/js/loginCtrl.js
+++ modules/login/js/loginCtrl.js
@@ -7,6 +7,7 @@ angular.module('BBApp').controller('loginCtrl', ['FBMSG', function (FBMSG) {
 		console.log('mockup of a callback')
 	}
 	self.signUp = function(){
+	firebase.auth(ref).createUserWithEmailAndPassword(self.email, self.password).catch(function(error) {
  //Handle Errors here.
   var errorCode = error.code;
   var errorMessage = error.message;
