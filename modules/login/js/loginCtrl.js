411d143121a5659d2795219665ecc1bc8b7aa7c2
ponydelrey <ann.cz3c@gmail.com>
2017-05-26 16:29:15 +0200

    Resset password added

--- modules/login/js/loginCtrl.js
+++ modules/login/js/loginCtrl.js
@@ -2,6 +2,15 @@ angular.module('BBApp').controller('loginCtrl', ['FBMSG', '$location','$scope', 
 
 	var ref = new Firebase(FBMSG);
 
+  $scope.resetPass = function(){
+  var email = $scope.email;
+
+  firebase.auth().sendPasswordResetEmail("annaczech93@gmail.com").then(function() {
+
+    window.alert('Sprawdź swoją skrzynkę pocztową. Otrzymałaś emaila z linkiem do zmiany hasła')
+     });
+  }
+
 
 	$scope.signUp = function(){
 		var email = $scope.email;
